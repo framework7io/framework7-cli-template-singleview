eee80350aca2fd0f5bb42e3cd17340aa43e2659a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-28 17:05:54 +0300

    condition script

--- scripts/f7-platform-condition.js
+++ scripts/f7-platform-condition.js
@@ -2,17 +2,30 @@ var fs = require('fs');
 var path = require('path');
 module.exports = function (ctx) {
     var projectRoot = ctx.opts.projectRoot;
-    var filesToReplace = [
-        'platforms/ios/www/index.html',
-        'platforms/android/assets/www/index.html'
-    ];
+    var walk = function(dir) {
+        var results = [];
+        var list = fs.readdirSync(dir);
+        list.forEach(function(file) {
+            file = dir + '/' + file;
+            var stat = fs.statSync(file);
+            if (stat && stat.isDirectory()) results = results.concat(walk(file));
+            else results.push(file);
+        });
+        return results;
+    };
+    var iosFiles = walk(path.join(projectRoot, 'platforms/ios/www'));
+    var androidFiles = walk(path.join(projectRoot, 'platforms/android/assets/www'));
+    var filesToReplace = [];
+    iosFiles.concat(androidFiles).forEach(function(filename) {
+        if (path.extname(filename) === '.html') filesToReplace.push(filename);
+    });
+    
     filesToReplace.forEach(function (filename) {
-        var fullFilePath = path.join(projectRoot, filename);
+        
         var isIos = filename.indexOf('platforms/ios/') >= 0;
-        if (!fs.existsSync(fullFilePath)) return;
-        var fileContent = fs.readFileSync(fullFilePath, 'utf-8');
+        if (!fs.existsSync(filename)) return;
+        var fileContent = fs.readFileSync(filename, 'utf-8');
         var conditions;
-
 
         var finalContent = '';
         if (isIos) {
@@ -40,6 +53,12 @@ module.exports = function (ctx) {
                 }
             });
         }
-        fs.writeFileSync(fullFilePath, finalContent);
+        finalContent = finalContent
+            .replace(/<!-- f7-if-ios -->/g, '')
+            .replace(/<!-- f7-endif-ios -->/g, '')
+            .replace(/<!-- f7-if-android -->/g, '')
+            .replace(/<!-- f7-endif-android -->/g, '');
+
+        fs.writeFileSync(filename, finalContent);
     });
 };
